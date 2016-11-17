3ea0f71f81fb4a59efaf763fdca42944100e6762
Itamar Efrat <Itamar.Efrat@hva.nl>
2016-11-17 22:46:35 +0100

    asset zooi

--- Spoopy/Assets/Itamar shit/Scripts/cleansingRoom.cs
+++ Spoopy/Assets/Itamar shit/Scripts/cleansingRoom.cs
@@ -7,8 +7,19 @@ public class cleansingRoom : MonoBehaviour {
     public Transform player;
     private float distancePlayer;
     public float minDistance = 10f;
+
+    //timers until exorcism
     private bool timerOn;
     public float timer = 10f;
+
+    //motion blur for each stage
+    public bool normal = true;
+    public bool first;
+    public bool second;
+    public bool third;
+    public bool fourth;
+    public bool fifth;
+    public GameObject motionblur;
 
 	// Use this for initialization
 	void Start () {
@@ -33,12 +44,41 @@ public class cleansingRoom : MonoBehaviour {
     {
         timer -= Time.deltaTime;
 
-        //effects on player
+        //effects on player connecting to the motion blur
+        if(timer == 10)
+        {
+            normal = true;
+        }
+        if(timer == 9 || timer == 8)
+        {
+            first = true;
+            normal = false;
 
+        }
+        if(timer == 7 || timer == 6)
+        {
+            second = true;
+            first = false;
+        }
+        if(timer == 5 || timer == 4)
+        {
+            third = true;
+            second = false;
+        }
+        if(timer == 3 || timer == 2)
+        {
+            fourth = true;
+            third = false;
+        }
+        if(timer <= 0)
+        {
+            fifth = true;
+            fourth = false;
+        }
 
-        if (timer <= 0)
+        /*if (timer <= 0)
         {
             Application.LoadLevel("GameOver");
-        }
+        }*/
     }
 }
