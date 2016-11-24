21b9f0f9368a8e7ef8d55705b645d9e3e8e2d2f5
Itamar Efrat <Itamar.Efrat@hva.nl>
2016-11-24 15:15:57 +0100

    verder met exorcist, motion blur half ish erin

---
+++ Spoopy/Assets/Itamar shit/Scripts/MotionBlurPlayer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class MotionBlurPlayer : MonoBehaviour {
+
+    //distance player and exorcist
+    public Transform exorcist;
+    private float distanceExorcist;
+    public float minDistance = 5f;
+
+    //timers until exorcism
+    private bool timerOn;
+    public float timer = 10f;
+
+    //motionblur;
+    public GameObject playerCam;
+    MotionBlur motionScript;
+
+    // Use this for initialization
+    void Start () {
+        distanceExorcist = 1000f;
+        Debug.Log(distanceExorcist);
+        timerOn = false;
+        motionScript = playerCam.GetComponent<MotionBlur>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        distanceExorcist = Vector3.Distance(playerCam.transform.position, exorcist.position);
+        Debug.Log(distanceExorcist);
+        if (distanceExorcist <= minDistance && timerOn == false)
+        {
+            timerOn = true;
+            blurr();
+        }
+        if (distanceExorcist > minDistance)
+        {
+            timerOn = false;
+            motionScript.blurAmount = 0.0f;
+        }
+    }
+
+    public void blurr()
+    {
+        if(distanceExorcist > minDistance)
+        {
+            motionScript.blurAmount = 0f;
+        }
+        else if (distanceExorcist <= minDistance && distanceExorcist > 4f)
+        {
+            motionScript.blurAmount = 0.5f;
+        }
+        else if(distanceExorcist <= 4f && distanceExorcist > 3f)
+        {
+            motionScript.blurAmount = 1.5f;
+        }
+        else if(distanceExorcist <= 3f && distanceExorcist > 2f)
+        {
+            motionScript.blurAmount = 4f;
+        }
+        else if(distanceExorcist <= 2f && distanceExorcist > 1f)
+        {
+            motionScript.blurAmount = 6.5f;
+        }
+        else if(distanceExorcist <= 1f && distanceExorcist > 0f)
+        {
+            motionScript.blurAmount = 10f;
+        }
+    }
+
+}
--- Spoopy/Assets/Itamar shit/Scripts/cleansingRoom.cs
+++ Spoopy/Assets/Itamar shit/Scripts/cleansingRoom.cs
@@ -2,83 +2,13 @@
 using System.Collections;
 
 public class cleansingRoom : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
 
-    //distance player and exorcist
-    public Transform player;
-    private float distancePlayer;
-    public float minDistance = 10f;
-
-    //timers until exorcism
-    private bool timerOn;
-    public float timer = 10f;
-
-    //motion blur for each stage
-    public bool normal = true;
-    public bool first;
-    public bool second;
-    public bool third;
-    public bool fourth;
-    public bool fifth;
-    public GameObject motionblur;
-
-	// Use this for initialization
-	void Start () {
-        timerOn = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        distancePlayer = Vector3.Distance(transform.position, player.position);
-        if(distancePlayer <= minDistance)
-        {
-            timerOn = true;
-            exorcism();
-        }
-        if (distancePlayer > minDistance)
-        {
-            timerOn = false;
-        }
     }
 
-    public void exorcism()
-    {
-        timer -= Time.deltaTime;
-
-        //effects on player connecting to the motion blur
-        if(timer == 10)
-        {
-            normal = true;
-        }
-        if(timer == 9 || timer == 8)
-        {
-            first = true;
-            normal = false;
-
-        }
-        if(timer == 7 || timer == 6)
-        {
-            second = true;
-            first = false;
-        }
-        if(timer == 5 || timer == 4)
-        {
-            third = true;
-            second = false;
-        }
-        if(timer == 3 || timer == 2)
-        {
-            fourth = true;
-            third = false;
-        }
-        if(timer <= 0)
-        {
-            fifth = true;
-            fourth = false;
-        }
-
-        /*if (timer <= 0)
-        {
-            Application.LoadLevel("GameOver");
-        }*/
+    // Update is called once per frame
+    void Update () {
+       
     }
 }
